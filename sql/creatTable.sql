2d499ff045ab9e0f1dc03d6f85c4bc548c1a7f07
weedsx <fyisgod@126.com>
2023-06-03 18:46:45 +0800

    sql修改

--- sql/creatTable.sql
+++ sql/creatTable.sql
@@ -1,20 +1,20 @@
 -- auto-generated definition
 create table user
 (
-    username     varchar(256)                       null comment '用户昵称',
-    id           bigint auto_increment comment 'id'
+    user_name     varchar(256)                       null comment '用户昵称',
+    id            bigint auto_increment comment 'id'
         primary key,
-    userAccount  varchar(256)                       null comment '账号',
-    avatarUrl    varchar(1024)                      null comment '用户头像',
-    gender       tinyint                            null comment '性别',
-    userPassword varchar(512)                       not null comment '密码',
-    phone        varchar(128)                       null comment '电话',
-    email        varchar(512)                       null comment '邮箱',
-    userStatus   int      default 0                 not null comment '状态 0 - 正常',
-    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
-    isDelete     tinyint  default 0                 not null comment '是否删除',
-    userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
-    planetCode   varchar(512)                       null comment '星球编号'
+    user_account  varchar(256)                       null comment '账号',
+    avatar_url    varchar(1024)                      null comment '用户头像',
+    gender        tinyint                            null comment '性别',
+    user_password varchar(512)                       not null comment '密码',
+    phone         varchar(128)                       null comment '电话',
+    email         varchar(512)                       null comment '邮箱',
+    user_status   int      default 0                 not null comment '状态 0 - 正常',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    is_delete     tinyint  default 0                 not null comment '是否删除',
+    user_role     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
+    planet_code   varchar(512)                       null comment '星球编号',
 )
     comment '用户';
